5c5b9ac2ede6d3f9034b1f54cf73a3e0f7a876c3
andrew <nazymko.patronus@gmail.com>
2014-12-18 23:12:29 +0200

    Initial commit

---
+++ src/main/java/com/sample/Config.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by andrew on 12/17/2014.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.companyname")
+public class Config extends WebMvcConfigurerAdapter {
+
+}
---
+++ src/main/java/com/sample/RootActivator.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+import java.util.Date;
+
+/**
+ * Created by andrew on 12/17/2014.
+ */
+public class RootActivator {
+    public Date rootHandler() {
+        return new Date();
+    }
+}
